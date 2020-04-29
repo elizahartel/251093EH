bdc385f2e3e2b7a62c764b7acc6073c400946507
elizahartel <251093@student.pwr.edu.pl>
2020-04-29 23:00:14 +0200

    Add files via upload

---
+++ PolowaZnakow.java
@@ -0,0 +1,14 @@
+package pl.hartel.firstapp;
+
+import java.util.Scanner;
+
+public class PolowaZnakow{
+
+    public int LiczPoloweZnakow(String slowo)
+    {
+        int a = slowo.length();
+        int b = a/2;
+        return b;
+    }
+
+}
